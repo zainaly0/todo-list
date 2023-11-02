e11c659de78966752a3941c6fe87177b40f5c1e4
Zaid Aly <85671033+zainaly0@users.noreply.github.com>
2023-11-02 15:26:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 // import TodoList from './Components/TodoList';
 import Todo from './Components/Todo.jsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TodoNav from './Components/TodoNav';
+
 
 
 function App() {
